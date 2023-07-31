00465a409a817b940afc9cfbc97024d8c2f116ae
bebetterwithstats <bebetterwithstats@gmail.com>
2023-07-31 16:17:41 +0200

    [DEV] stocks.py et youtube-downloader.py

--- src/english-exam.py
+++ src/english-exam.py
@@ -58,35 +58,55 @@ fr_words = build_words_list(ROOT + files[int(selected_file)], LANGUE.index("FR")
 # STEP 2 : test knowledge
 score = 0
 
-type_an_answer("\n## STEP 1 ## Are you ready ?")
+type_an_answer("\n## STEP 2 ## Are you ready ?")
 
 for i in range(10):
 	selected_language = random.randrange(len(LANGUE))
 	selected_word = random.randrange(len(en_words))
 	
+	# if ENGLISH
 	if (selected_language == LANGUE.index("EN")):
 
-		sentence = "\nTranslate '" + en_words[selected_word] + "' (EN) :\n>> "
-		if (type_an_answer(sentence).lower() == fr_words[selected_word].lower()):
+		sentence = "\nTranslate in french '" + en_words[selected_word] + "' :\n>> "
+		answer = type_an_answer(sentence)
+		if (answer.lower() == fr_words[selected_word].lower()):
 			print("*** Yes !")
-			score = score + 1
+			score = score + 2
+		elif (answer.lower() == 'help'):
+			sentence = "it starts with a '" + fr_words[selected_word][0] + "' :\n>> "
+			answer = type_an_answer(sentence)
+			if (answer.lower() == fr_words[selected_word].lower()):
+				print("*** Yes !")
+				score = score + 1
+			else:
+				print("*** The answer was " + fr_words[selected_word])
 		else:
 			print("*** The answer was " + fr_words[selected_word])
 
+	# if FRENCH
 	elif (selected_language == LANGUE.index("FR")):
 		
-		sentence = "\nTranslate '" + fr_words[selected_word] + "' (FR) :\n>> "
-		if (type_an_answer(sentence).lower() == en_words[selected_word].lower()):
+		sentence = "\nTranslate in english '" + fr_words[selected_word] + "' :\n>> "
+		answer = type_an_answer(sentence)
+		if (answer.lower() == en_words[selected_word].lower()):
 			print("*** Yes !")
-			score = score + 1
+			score = score + 2
+		elif (answer.lower() == 'help'):
+			sentence = "it starts with a '" + en_words[selected_word][0] + "' :\n>> "
+			answer = type_an_answer(sentence)
+			if (answer.lower() == en_words[selected_word].lower()):
+				print("*** Yes !")
+				score = score + 1
+			else:
+				print("*** The answer was " + en_words[selected_word])
 		else:
-			# exception coding error, these case will never happened
 			print("*** The answer was " + en_words[selected_word])
 
 	else:
+		# exception coding error, these case will never happened
 		print("[ERROR] language does not exist")
 
 # STEP 3 : print score
-print("\n\n## FINAL SCORE ## %d / 10\n\n" % score)
+print("\n\n## FINAL SCORE ## %d / 20\n\n" % score)
 
 # FIN -- MAIN CODE
