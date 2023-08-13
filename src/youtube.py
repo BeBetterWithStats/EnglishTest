9470b60753181a248ff0659854fcf95e1af48248
bebetterwithstats <bebetterwithstats@gmail.com>
2023-08-13 15:25:31 +0200

    [DEV] ajout d'un controle de l'url saisie à l'iade d'une REGEX

--- src/youtube.py
+++ src/youtube.py
@@ -3,6 +3,7 @@
 # IMPORTS
 
 import os
+import re
 from pytube import YouTube
 from sys import argv
 from datetime import date
@@ -48,26 +49,50 @@ def print_executionTime(_start, _finish):
 
 ##########################################################################
 
-PATH = "/Users/alexandrelods/Downloads/youtube_dl"
+def _is_urlValide(_url):
+	# https://youtu.be/MLu3I8tjKEg
+	# https://www.youtube.com/live/I4eVdFPjVgA?feature=share
+	condition = re.search(r"^(https|http):\/\/[www.]*youtu[a-zA-Z0-9_.=?-]+", _url)
+	# condition = re.search(r"^(https|http):\/\/[a-zA-Z0-9.]*youtu[a-zA-Z0-9_.=?-]+", _url)
+	return True if condition else False
 
-start = start_program()
+############ MAIN #############
+def main():
+	PATH = "/Users/alexandrelods/Downloads/youtube_dl"
+
+	start = start_program()
 
 
-if len(argv) != 2:
-    link = input("Quelle est l'URL de la vidéo youtube à télécharger ? ").strip()
-else :
-    link = argv[1].strip
+	if len(argv) != 2:
+		link = input("Quelle est l'URL de la vidéo youtube à télécharger ? ").strip()
+	else :
+		link = argv[1].strip
 
-# TODO ajouter un controle de l'url avec une REGEX
+	# TODO ajouter un controle de l'url avec une REGEX
+	# https://youtu.be/MLu3I8tjKEg
+	# https://www.youtube.com/live/I4eVdFPjVgA?feature=share
+	if not _is_urlValide(link):
+		return print(f"[ERROR] le lien de téléchargement '{link}' n'est pas valide")
 
-print(f"[INFO] Téléchargement de la vidéo '{link}'")
 
-yt = YouTube(link)
-print("[INFO] Titre = '" + yt.title + "'")
-print("[INFO] Auteur = '" + yt.author + "'")
+	print(f"[INFO] Téléchargement de la vidéo '{link}'")
 
-yt.streams.get_highest_resolution().download(PATH)
-print(f"[INFO] Vidéo disponible dans le répertoire '{PATH}'")
+	yt = YouTube(link)
+	print("[INFO] Titre = '" + yt.title + "'")
+	print("[INFO] Auteur = '" + yt.author + "'")
 
-end = end_program()
-print_executionTime(start, end)
+	yt.streams.get_highest_resolution().download(PATH)
+	print(f"[INFO] Vidéo disponible dans le répertoire '{PATH}'")
+
+	end = end_program()
+	print_executionTime(start, end)
+
+	return True
+############ FIN - MAIN #############
+
+
+if __name__ == "__main__":
+
+	link = input("Quelle est l'URL de la vidéo youtube à télécharger ? ").strip()
+	print(_is_urlValide(link))
+	# main()
